838fcd5a076c9dbaac90324c678a22a1f0194c0b
卢忠伟 <lucodestation@163.com>
2021-12-13 00:09:26 +0800

    获取微信用户信息和微信用户绑定的用户手机号码

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ App({
   // 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
   onLaunch: function () {
     console.log('App onLaunch')
+
+    wx.login({
+      success(result) {
+        console.log(result)
+        const { code } = result
+
+        wx.request({
+          url: 'http://localhost:3000/api/auth/code2Session',
+          data: { code },
+          success(response) {
+            console.log(response.data)
+            const { sessionKey } = response.data
+            wx.setStorageSync('sessionKey', sessionKey)
+          },
+        })
+      },
+    })
   },
 
   // 当小程序启动，或从后台进入前台显示，会触发 onShow
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,101 @@ Page({
   // 页面的初始数据
   data: {},
 
+  // 获取微信用户绑定的手机号
+  handleGetPhoneNumber(event) {
+    console.log(event.detail)
+    if (event.detail.errMsg !== 'getPhoneNumber:ok') return
+
+    // 包括敏感数据在内的完整用户信息的加密数据
+    const { encryptedData } = event.detail
+    // 加密算法的初始向量
+    const { iv } = event.detail
+    // 敏感数据对应的云 ID，开通云开发的小程序才会返回，可通过云调用直接获取开放数据
+    const { cloudID } = event.detail
+    console.log({ encryptedData, iv, cloudID })
+
+    const sessionKey = wx.getStorageSync('sessionKey')
+
+    wx.request({
+      url: 'http://localhost:3000/api/auth/phoneNumber',
+      data: { sessionKey, encryptedData, iv },
+      success(response) {
+        console.log(response.data)
+        wx.setStorageSync('userPhoneNumber', response.data.phoneNumber)
+      },
+    })
+
+    // wx.checkSession({
+    //   success: (res) => {
+    //     console.log('已登录', res)
+    //     const code = wx.getStorageSync('code')
+    //     wx.request({
+    //       url: 'http://localhost:3000/api/auth/code2Session',
+    //       data: { encryptedData, iv, appId, appSecret, code },
+    //       success(response) {
+    //         console.log(response.data)
+    //       },
+    //     })
+    //   },
+    //   fail(err) {
+    //     console.log('未登录', err)
+    //     wx.login({
+    //       success(result) {
+    //         console.log(result)
+    //         const { code } = result
+    //         // 解密需要 5 个参数
+    //         // encryptedData
+    //         // iv
+    //         // appId
+    //         // appSecret
+    //         // code
+
+    //         wx.setStorageSync('code', code)
+
+    //         wx.request({
+    //           url: 'http://localhost:3000/api/auth/code2Session',
+    //           data: { encryptedData, iv, appId, appSecret, code },
+    //           success(response) {
+    //             console.log(response.data)
+    //           },
+    //         })
+    //       },
+    //     })
+    //   },
+    // })
+  },
+
+  // 测试按钮
+  handleGetUserInfo() {
+    console.log('测试按钮')
+    wx.getUserInfo({
+      success(result) {
+        console.log('getUserInfo', result)
+      },
+    })
+  },
+
+  handleGetUserProfile() {
+    wx.getUserProfile({
+      desc: 'desc',
+      success(result) {
+        console.log('getUserProfile', result)
+        const { encryptedData, iv } = result
+
+        const sessionKey = wx.getStorageSync('sessionKey')
+
+        wx.request({
+          url: 'http://localhost:3000/api/auth/profile',
+          data: { sessionKey, encryptedData, iv },
+          success(response) {
+            console.log(response.data)
+            wx.setStorageSync('userProfile', response.data)
+          },
+        })
+      },
+    })
+  },
+
   // 生命周期函数--监听页面加载
   onLoad: function (options) {
     console.log('/pages/index/index onLoad')
